7c787c1c6fe58900a425f1bfa3522f8b1ad4ac1d
Jacob Robinson <Jacobrobinson4321@gmail.com>
2025-11-26 16:08:02 -0500

    Send invite to guest
    When guest is selected from the guest list an invite request is sent to the server.
    
    Also ensure that the client is initialized properly before sending any other requests to the server.

--- UnityVRTest/Assets/Scripts/Host/GuestList.cs
+++ UnityVRTest/Assets/Scripts/Host/GuestList.cs
@@ -7,18 +7,25 @@ using UnityEngine.UI;
 
 public class GuestList : MonoBehaviour
 {
-    public List<Device> devices;
+    public List<Guest> guests;
     public Backend backend;
     public ScrollRect list;
     public GameObject listItem;
 
+    private void Start()
+    {
+        RefreshGuestList();    
+    }
+
     public async void RefreshGuestList()
     {
         // Update the list
-        devices = await backend.GetGuestList();
-        Debug.Log(devices[0].name);
+        guests = await backend.GetGuestList();
 
-        DisplayGuestList();
+        if (guests != null)
+        {
+            DisplayGuestList();
+        }
     }
 
     // Display the guest list in the scroll rect
@@ -32,14 +39,28 @@ public class GuestList : MonoBehaviour
 
         // Repopulate it with the updated items
         float currentYOffset = 0;
-        foreach (Device device in devices)
+        foreach (Guest guest in guests)
         {
+            // Create new list item for this guest
             GameObject newListItem = GameObject.Instantiate(listItem, list.content, false);
             RectTransform t = newListItem.GetComponent<RectTransform>();
+
+            // Set list item's position
             t.position = new Vector3(t.position.x, t.position.y + currentYOffset, t.position.z);
             currentYOffset -= t.rect.height;
+
             // Set the text display to be the device name
-            newListItem.GetComponentInChildren<TMP_Text>().text = device.name;
+            newListItem.GetComponentInChildren<TMP_Text>().text = guest.name;
+
+            // Set onclick to send an invite for this guest
+            newListItem.GetComponent<Button>().onClick.AddListener(async () =>
+            {
+                bool success = await backend.SendInviteToGuest(guest.id);
+                if (success)
+                {
+                    Debug.Log($"Now linked to guest(ID={guest.id},NAME={guest.name})");
+                }
+            });
         }
     }
 }
--- UnityVRTest/Assets/Scripts/Networking/Backend.cs
+++ UnityVRTest/Assets/Scripts/Networking/Backend.cs
@@ -1,13 +1,15 @@
-using UnityEngine;
 using Microsoft.AspNetCore.SignalR.Client;
-using System.Threading.Tasks;
-using System;
-using NUnit.Framework;
-using System.Collections.Generic;
 using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
 
 [System.Serializable]
-public class Device
+public class Guest
 {
     public string name { get; set; }
     public string id { get; set; }
@@ -25,8 +27,10 @@ public class Backend : MonoBehaviour
     string port = "8080";
 
     private HubConnection connection;
-   
-    async void Start()
+    private bool isConnected = false;
+    private Task initTask = null;
+
+    private async void Awake()
     {
         // Build the connection
         connection = new HubConnectionBuilder()
@@ -37,7 +41,9 @@ public class Backend : MonoBehaviour
 
         try
         {
-            await connection.InvokeAsync("InitDevice", deviceName, isHost);
+            await connection.InvokeAsync<Task>("InitDevice", deviceName, isHost);
+            Debug.Log("Initialized with server!");
+            isConnected = true;
         }
         catch (System.Exception e)
         {
@@ -45,17 +51,55 @@ public class Backend : MonoBehaviour
         }
     }
 
-    public async Task<List<Device>> GetGuestList()
+    // If not initialized yet wait until either we are or too much time passes.
+    private async Task WaitForInit(float timeout = 0.5f)
     {
+        float totalWaitingTime = 0.0f;
+        while (!isConnected) 
+        { 
+            Debug.Log("Waiting for init...");
+            totalWaitingTime += Time.deltaTime;
+            if (totalWaitingTime > timeout) { break; }
+            await Task.Yield();
+        }
+        if (!isConnected) { throw new Exception("This client is not initialized with the server yet."); }
+    }
+
+    public async Task<List<Guest>> GetGuestList()
+    {
+        // Make sure that all the initialization has been completed before continuing
         try
         {
+            await WaitForInit();
+
             string response = await connection.InvokeAsync<string>("RequestServerData", "GUESTS");
-            return JsonConvert.DeserializeObject<List<Device>>(response);
+            return JsonConvert.DeserializeObject<List<Guest>>(response);
         }
         catch (System.Exception e)
         {
             Debug.LogError("Failed to retrieve guest list: " + e.Message);
-            return null;
+            return new List<Guest>();
+        }
+    }
+
+    /// <summary>
+    /// Send an invite to the guest with the provided id.
+    /// </summary>
+    /// <param name="guestId">The id associated with the guest we want to send an invite.</param>
+    /// <returns>True if the link succeeds and false otherwise.</returns>
+    public async Task<bool> SendInviteToGuest(string guestId)
+    {
+        try
+        {
+            await WaitForInit();
+
+            await connection.InvokeAsync("RequestDeviceLink", guestId);
+            return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError("Failed to invite guest: " + e.Message);
+            return false;
         }
     }
 
